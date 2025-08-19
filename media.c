bb7678fe914c9e1c4c06f4a980e6937ee5bb4852
Solano-tech <202502014791@alunos.estacio.br>
2025-08-19 06:44:54 +0000

    Estudo de conversao explicita e implicita

--- media.c
+++ media.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+//Estudar conversão explicita e implicita atraves do calculo de media
+
+int main(){
+
+    float nota1, nota2, nota3;
+    float media;
+    printf("*** Cálculo da Média ***\n");
+    
+    printf("Digite a sua primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a sua segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Digite a sua terceira nota: ");
+    scanf("%f", &nota3);
+
+//    media = (float)(nota1 + nota2 + nota3) / 3; // Usando casting
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("Sua média é %.1f\n", media);
+
+    return 0;
+}
