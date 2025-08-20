b5ff2c8827fa35881c09d570c0e05513c9a194ef
Solano-tech <202502014791@alunos.estacio.br>
2025-08-20 03:09:12 +0000

    Ajuste no comentário

--- media.c
+++ media.c
@@ -4,6 +4,7 @@
 
 int main(){
 
+//    int nota1, nota2, nota3;
     float nota1, nota2, nota3;
     float media;
     printf("*** Cálculo da Média ***\n");
@@ -17,7 +18,7 @@ int main(){
     printf("Digite a sua terceira nota: ");
     scanf("%f", &nota3);
 
-//    media = (float)(nota1 + nota2 + nota3) / 3; // Usando casting
+//    media = (float)(nota1 + nota2 + nota3) / 3; // Usando casting e convertendo as variáveis antes do cálculo
     media = (nota1 + nota2 + nota3) / 3;
 
     printf("Sua média é %.1f\n", media);
