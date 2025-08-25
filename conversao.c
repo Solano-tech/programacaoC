be219c40dc3c26e8f9df6cf9bbdc884ed9543820
Solano-tech <202502014791@alunos.estacio.br>
2025-08-25 21:00:40 +0000

    Estudo de conversão de variáveis

--- conversao.c
+++ conversao.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(){
+    
+    //estudo de conversão explicita e implicita de variaveis
+
+    int a = 5;
+    int b = 12;
+    int x = 5;
+    int y = 12;
+    int soma = x + y;
+    int diferenca = y - x;
+    float multiplicacao = x * y;
+    float divisao = y / x; // nesta divisao o resultado é truncado, mostra apenas o valor inteiro
+    float quociente = (float) b / a; // nesta divisao, mostra o resultado de forma decimal, completo
+
+    printf("Soma: %d \n", soma);
+    printf("Diferença: %d \n", diferenca);
+    printf("Multiplicação: %.2f \n", multiplicacao);
+    printf("Divisão: %.2f \n", divisao);
+    printf("Divisão: %.2f \n", quociente);
+
+    return 0;
+}
