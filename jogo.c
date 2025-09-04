add8e3842f39a74fd68d381a0763d8454e51f048
Solano Andrade <202502014791@alunos.estacio.br>
2025-09-04 00:13:21 +0000

    criado jogo jokenpo

--- jogo.c
+++ jogo.c
@@ -4,6 +4,7 @@
 
 int main() {
 
+    /*
     //JOGO PARA ADIVINHAR NÚMEROS
 
     int opcao, numeroSecreto, palpite; //Definição de variáveis
@@ -43,10 +44,70 @@ int main() {
         case 3:
         printf("Saindo...\n");
         break;
-        
+
         default:
             printf("Você digitou uma opção inválida!\n");
         break;
     }
+    */
+
+    // JOGO JOKENPO
+
+    int escolhaJogador, escolhaComputador; //declaração variáveis
+
+    srand(time(0));
+
+    printf("--- JOGO DE JOKENPO ---\n");
+    printf("Escolha uma opção:\n");
+    printf("1. Pedra\n");
+    printf("2. Papel\n");
+    printf("3. Tesoura\n");
+    printf("Escolha: ");
+    scanf("%d", &escolhaJogador);
+
+    escolhaComputador = rand() % 3 + 1;
+
+    switch (escolhaJogador) {
+        case 1:
+        printf("Jogador: Pedra - ");
+        break;
+
+        case 2:
+        printf("Jogador: Papel - ");
+        break;
+
+        case 3:
+        printf("Jogador: Tesoura - ");
+        break;
+
+        default:
+        printf("Opção inválida!\n");
+        break;
+    }
+
+    switch (escolhaComputador) {
+        case 1:
+        printf("Computador: Pedra.\n");
+        break;
+
+        case 2:
+        printf("Computador: Papel. \n");
+        break;
+
+        case 3:
+        printf("Computador: Tesoura. \n");
+        break;
+    }
+
+    if (escolhaComputador == escolhaJogador) {
+        printf(" O JOGO EMPATOU!\n");
+    } else if ((escolhaJogador == 1) && (escolhaComputador == 3) || 
+                (escolhaJogador == 2) && (escolhaComputador == 1) || 
+                (escolhaJogador == 3) && (escolhaComputador == 2)) {
+        printf(" PARABÉNS! VOCÊ GANHOU.\n");
+    } else {
+        printf(" VOCÊ PERDEU!\n");
+    }
+
     return 0;
 }
