27fd99b0e18dc5ccfd4837e3ed581afe8691fffd
Solano Andrade <202502014791@alunos.estacio.br>
2025-09-06 08:47:48 +0000

    teste operador ternario

--- media.c
+++ media.c
@@ -7,6 +7,8 @@ int main(){
 //    int nota1, nota2, nota3;
     float nota1, nota2, nota3;
     float media;
+    char* resultado;
+
     printf("*** Cálculo da Média ***\n");
     
     printf("Digite a sua primeira nota: ");
@@ -22,6 +24,21 @@ int main(){
     media = (nota1 + nota2 + nota3) / 3;
 
     printf("Sua média é %.1f\n", media);
+
+//     Se média maior que 6, aprovado
+    resultado = (media >= 6) ? printf("Aprovado!\n"): printf("Reprovado!\n");
+    //if (media >= 6){
+    //    printf("Aprovado!");
+    //} else{
+    //    printf("Reprovado!");
+    //}
+
+    switch(opcao){
+        case 'M'
+        case 'm'
+            \\codigo
+        break;
+    }
 
     return 0;
 }
